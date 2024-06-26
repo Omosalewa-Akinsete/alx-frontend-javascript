4b8556e6dd93876440df7e6e47174e59ab51bf37
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-26 12:57:12 -0700

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,17 @@
-import signUpUser from './4-user-promise';
+import signUpcreatedUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ];
-
-    const results = await Promise.allSettled(promises);
-
-    return results.map((result) => (
-        {
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-        }
-    ));
+  const promise = [];
+  try {
+    const User = await signUpcreatedUser(firstName, lastName);
+    promise.push({ status: 'fulfilled', value: User });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    promise.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return promise;
 }
