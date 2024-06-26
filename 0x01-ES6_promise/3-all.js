2ae6257c19602d734c4eef2ac20db1e7b894db26
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-26 09:58:15 -0700

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,12 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  let user;
-  let firstName;
-  let lastName;
-
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => {
-      user = res[0].body;
-      firstName = res[1].firstName;
-      lastName = res[1].lastName;
+export function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`body ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => { console.log('Signup system offline'); });
+    .catch((error) => {
+      console.error('Signup system offline');
+    });
 }
