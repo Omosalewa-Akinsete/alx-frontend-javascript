d71d0a97d57a29347bdc002075c42e3669314288
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-26 07:53:21 -0700

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
+    .then(response => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
     })
-    .catch((error) => {
+    .catch(error => {
       console.error(error);
       return new Error();
     });
