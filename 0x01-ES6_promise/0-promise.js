6953ef09d0c72853107774bc25bd548bd79c7079
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-25 21:37:36 -0700

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Your API logic here
+    // Call resolve(data) if the API call is successful
+    // Call reject(error) if there is an error
+  });
+}
+
+module.exports = getResponseFromAPI;
