c887d221f7836602fcd2ab2f603e4838e9ef0267
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-27 07:02:36 -0700

    Let us make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = [];
-
-  for (let i = 0; i < roomSizes.length; i++) {
-    const room = new ClassRoom(roomSizes[i]);
-    rooms.push(room);
-  }
-
-  return rooms;
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
-
-export default initializeRooms;
