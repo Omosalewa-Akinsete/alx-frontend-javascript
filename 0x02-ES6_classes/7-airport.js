3d96b3a2b573e5daee2cdf8c337da15d4a2c65c2
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-27 13:09:38 -0700

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,18 +1,17 @@
 export default class Airport {
   constructor(name, code) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
+
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
