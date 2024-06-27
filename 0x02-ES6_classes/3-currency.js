c5e9f6a9043a3db66377d8a398be3fd6cb2c9a7b
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-27 08:19:05 -0700

    Methods, static methods, computed methods names. MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
