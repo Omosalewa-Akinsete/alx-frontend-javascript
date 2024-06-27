2be8cf3751583ff813c8745f376454b7a9af01c1
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-06-27 14:39:07 -0700

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,46 @@
-import Car from './10-car';
+const carBrand = Symbol('brand');
+const carMotor = Symbol('motor');
+const carColor = Symbol('color');
 
-export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
+export default class Car {
+  constructor(brand, motor, color) {
+    this[carBrand] = brand;
+    this[carMotor] = motor;
+    this[carColor] = color;
+  }
 
-        this._range = range;
-    }
+  cloneCar() {
+    return new Car(this[carBrand], this[carMotor], this[carColor]);
+  }
 
-    // eslint-disable-next-line class-methods-use-this
-    cloneCar() {
-        return new Car();
-    }
+  get brand() {
+    return this[carBrand];
+  }
+
+  get motor() {
+    return this[carMotor];
+  }
+
+  get color() {
+    return this[carColor];
+  }
+}
+
+export class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this[carBrand], this[carMotor], this[carColor]);
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
 }
