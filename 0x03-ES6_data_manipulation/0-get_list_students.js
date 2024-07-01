f5fb17aaa10cbc7d785d2fb4a878eb587a365127
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-01 16:11:15 -0700

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,23 @@
-const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
+export default function getListStudents() {
+  const arr = [];
 
-export default getListStudents;
+  arr[0] = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  arr[1] = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  arr[2] = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return arr;
+}
