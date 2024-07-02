9f70a8c7ea10dcf31b9d7802c386202b598472d0
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-02 13:50:13 -0700

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+  if (position > length) {
     throw new Error('Position outside range');
   }
-
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
-  view.setInt8(position, value);
-  return view;
+  dataView.setInt8(position, value);
+  return dataView;
 }
