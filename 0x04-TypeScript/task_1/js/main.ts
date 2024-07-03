13d63d9baafdd4544dc6ffb5737b3719ca97aab5
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-03 15:26:05 -0700

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,27 +1,46 @@
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  FullTimeEmployee: boolean;
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
   yearsOfExperience?: number;
-  location: string;
+  readonly location: string;
   [key: string]: any;
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
 
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstInitial}. ${lastName}`;
+
+  return fullName;
+}
+
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  const firstLetter = firstName.charAt(0).toUpperCase();
-  const fullName = `${firstLetter}. ${lastName}`;
-  return fullName;
-};
-
-console.log(printTeacher("John", "Doe"));
 
 interface StudentClassConstructor {
   new (firstName: string, lastName: string): StudentClass;
@@ -36,7 +55,7 @@ class StudentClass implements StudentClass {
   constructor(private firstName: string, private lastName: string) {}
 
   workOnHomework(): string {
-    return "Currently working";
+    return 'Currently working';
   }
 
   displayName(): string {
