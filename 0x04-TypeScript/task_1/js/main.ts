8472a36389c5b10218006f3fb909ce9232a3b83a
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-03 08:49:17 -0700

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,12 @@
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
-  readonly FullTimeEmployee: boolean;
+  FullTimeEmployee: boolean;
   yearsOfExperience?: number;
-  readonly location: string;
+  location: string;
   [key: string]: any;
 }
 
-const director1: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: true,
-  location: 'London',
-  contrcat: true,
-};
-
-console.log(teacher1);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
